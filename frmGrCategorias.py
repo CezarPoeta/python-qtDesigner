4fb755bc21b600a21e776b1474b0360d84124d2e
Cezar Roberto Poeta <cezarrobertopoeta@gmail.com>
2021-08-11 22:43:43 -0300

    update
    updated

--- frmGrCategorias.py
+++ frmGrCategorias.py
@@ -1,13 +1,22 @@
+import sqlite3
+from sqlite3 import Error
+from sqlite3.dbapi2 import connect
 from PyQt5 import uic, QtWidgets
+
+banco = connect('SysFinanCtrl.db')
+cursor = banco.cursor()
+
+cursor.execute('SELECT * FROM tbgrcategorias')
 
 def Bloqueia():
     formulario.txtNome.setStyleSheet('QLineEdit {font:bold; font-size:20px; background-color:rgb(182, 182, 182)}')
     formulario.btnConfirmar.setStyleSheet('QPushButton {background-color: rgb(177, 180, 197); color: rgb(134, 134, 134)}')
     formulario.btnSair.setStyleSheet('QPushButton {background-color: rgb(177, 180, 197); color: rgb(134, 134, 134)}')
 
-def CarregarGrCategorias():
-    print('Carregar o QTableWidget!!!') 
 
+def CarregarGrCategorias():
+    print(cursor.fetchall())
+    print('Funcionou, vou dormmir agora!')
 
 
 app = QtWidgets.QApplication([])
@@ -17,6 +26,6 @@ formulario=uic.loadUi('frmGrCategorias.ui')
 Bloqueia()
 CarregarGrCategorias() 
 
-formulario.show()
+#formulario.show()
 
 app.exec()
